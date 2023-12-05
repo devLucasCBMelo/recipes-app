a5a4e66c6acfecef743039cd9f77a120cd14e1b9
Julio Matias <juliocesark2@hotmail.com>
2023-12-04 22:30:13 -0300

    testes do contex finalizados.

--- src/tests/Header.test.tsx
+++ src/tests/Header.test.tsx
@@ -1,14 +1,22 @@
-import { screen, act } from '@testing-library/react';
+import { screen, act, waitFor } from '@testing-library/react';
+import { vi } from 'vitest';
 import { renderWithRouter } from '../utils/renderWithRouter';
 import Header from '../components/Header/Header';
 import SearchBarProvider from '../contex/SearchBarProvider';
 import App from '../App';
+import DefaultIcon from '../images/logo.png';
 
 const search = 'search-input';
+const nameRadioId = 'name-search-radio';
+const ingredientRadioId = 'ingredient-search-radio';
+const firstLetterRadioId = 'first-letter-search-radio';
 
 describe('Testes Header', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
   it('Testa  Header na page "Meals', async () => {
-    const { user } = renderWithRouter(<Header namePage="Meals" />, { route: '/meals' });
+    const { user } = renderWithRouter(<Header pageIcon={ DefaultIcon } namePage="Meals" />, { route: '/meals' });
     const meals = screen.getByRole('heading', {
       name: /meals/i,
     });
@@ -28,7 +36,7 @@ describe('Testes Header', () => {
   });
 
   it('Testa o Header na page "Profile"', () => {
-    renderWithRouter(<Header namePage="Profile" />, { route: '/profile' });
+    renderWithRouter(<Header pageIcon={ DefaultIcon } namePage="Profile" />, { route: '/profile' });
     const profile = screen.getByRole('heading', {
       name: /profile/i,
     });
@@ -36,7 +44,7 @@ describe('Testes Header', () => {
   });
 
   it('Testa o Header na page "Drinks"', async () => {
-    const { user } = renderWithRouter(<Header namePage="Drinks" />, { route: '/drinks' });
+    const { user } = renderWithRouter(<Header pageIcon={ DefaultIcon } namePage="Drinks" />, { route: '/drinks' });
     const drinks = screen.getByRole('heading', {
       name: /drinks/i,
     });
@@ -55,7 +63,7 @@ describe('Testes Header', () => {
   });
 
   it('Testa o Header na page "Done Recipes"', async () => {
-    const { user } = renderWithRouter(<Header namePage="Done Recipes" />, { route: '/done-recipes' });
+    const { user } = renderWithRouter(<Header pageIcon={ DefaultIcon } namePage="Done Recipes" />, { route: '/done-recipes' });
     const doneRec = screen.getByRole('heading', {
       name: /done recipes/i,
     });
@@ -68,7 +76,7 @@ describe('Testes Header', () => {
     await user.click(profileButton);
   });
   it('Testa o Header na page "Meals" se o SearchBar aparece', async () => {
-    const { user } = renderWithRouter(<Header namePage="Meals" />, { route: '/meals' });
+    const { user } = renderWithRouter(<Header pageIcon={ DefaultIcon } namePage="Meals" />, { route: '/meals' });
     const meals = screen.getByRole('heading', {
       name: /meals/i,
     });
@@ -101,7 +109,7 @@ describe('Testes Header', () => {
     await user.type(searchInput, inputType);
 
     // pega o radio button de ingrediente
-    const ingredientRadio = screen.getByTestId('ingredient-search-radio');
+    const ingredientRadio = screen.getByTestId(ingredientRadioId);
     await user.click(ingredientRadio);
 
     const searchButton = screen.getByRole('button', { name: /search/i });
@@ -112,7 +120,7 @@ describe('Testes Header', () => {
 
     await user.type(searchInput, inputType);
 
-    const nameRadio = screen.getByTestId('name-search-radio');
+    const nameRadio = screen.getByTestId(nameRadioId);
     await user.click(nameRadio);
 
     await user.click(searchButton);
@@ -130,7 +138,7 @@ describe('Testes Header', () => {
     expect(await screen.findByText(/apple frangipan tart/i)).toBeInTheDocument();
   });
 
-  it('Caso apenas uma comida seja encontrada, deve-se ir para sua rota de detalhes', async () => {
+  it('Caso apenas um nome comida seja encontrada na pesquisa do SearchBar do Meals, deve-se ir para sua rota de detalhes', async () => {
     const { user } = renderWithRouter(
       <SearchBarProvider>
         <App />
@@ -145,7 +153,7 @@ describe('Testes Header', () => {
 
     const inputType = 'Arrabiata';
 
-    const nameRadio = screen.getByTestId('name-search-radio');
+    const nameRadio = screen.getByTestId(nameRadioId);
 
     const searchButton = screen.getByRole('button', { name: /search/i });
 
@@ -156,5 +164,215 @@ describe('Testes Header', () => {
       await user.click(nameRadio);
       await user.click(searchButton);
     });
+
+    expect(await screen.findByRole('heading', { name: /spicy arrabiata penne/i })).toBeInTheDocument();
+    const { pathname } = window.location;
+    expect(pathname).toBe('/meals/52771');
+  });
+
+  it('Verifica se ao passar um ingrediente que não existe no SearchBar, é exibido um alerta, na pagina Meals', async () => {
+    const alertMock = vi.spyOn(window, 'alert').mockImplementation(() => {});
+    const { user } = renderWithRouter(
+      <SearchBarProvider>
+        <App />
+      </SearchBarProvider>,
+      { route: '/meals' },
+    );
+
+    const searchBtn = screen.getByRole('img', { name: /pesquisar/i });
+
+    await user.click(searchBtn);
+
+    const searchInput = screen.getByTestId(search);
+
+    const inputInvalid = 'invalid';
+
+    const ingredientRadio = screen.getByTestId(ingredientRadioId);
+
+    const searchButton = screen.getByRole('button', { name: /search/i });
+
+    await act(async () => {
+      await user.type(searchInput, inputInvalid);
+      await user.click(ingredientRadio);
+      await user.click(searchButton);
+    });
+
+    await waitFor(() => expect(alertMock).toHaveBeenCalledWith(
+      "Sorry, we haven't found any recipes for these filters",
+    )); // verifica se apareceu o alerta
+  });
+
+  it('Testa se o SearchBar na pagina "drinks" faz a pesquisa corretamente', async () => {
+    const { user } = renderWithRouter(
+      <SearchBarProvider>
+        <App />
+      </SearchBarProvider>,
+      { route: '/drinks' },
+    );
+    const searchBtn = screen.getByRole('img', { name: /pesquisar/i });
+
+    await user.click(searchBtn);
+
+    const searchInput = screen.getByTestId(search);
+
+    const inputType = 'Gin';
+
+    await user.type(searchInput, inputType);
+
+    // pega o radio button de ingrediente
+    const ingredientRadio = screen.getByTestId(ingredientRadioId);
+    await user.click(ingredientRadio);
+
+    const searchButton = screen.getByRole('button', { name: /search/i });
+
+    await user.click(searchButton);
+
+    expect(await screen.findByText(/3-mile long island iced tea/i)).toBeInTheDocument();
+
+    await user.type(searchInput, inputType);
+
+    const nameRadio = screen.getByTestId(nameRadioId);
+    await user.click(nameRadio);
+
+    await user.click(searchButton);
+
+    expect(await screen.findByText(/gin fizz/i)).toBeInTheDocument();
+
+    const inputType2 = 'a';
+    await user.type(searchInput, inputType2);
+
+    const firstLetterRadio = screen.getByTestId(firstLetterRadioId);
+    await user.click(firstLetterRadio);
+
+    await user.click(searchButton);
+
+    expect(await screen.findByText(/a1/i)).toBeInTheDocument();
+  });
+
+  it('Caso apenas um nome de bebida seja encontrada na pesquisa do SearchBar do Drinks, deve-se ir para sua rota de detalhes', async () => {
+    const { user } = renderWithRouter(
+      <SearchBarProvider>
+        <App />
+      </SearchBarProvider>,
+      { route: '/drinks' },
+    );
+    const searchBtn = screen.getByRole('img', { name: /pesquisar/i });
+
+    await user.click(searchBtn);
+
+    const searchInput = screen.getByTestId(search);
+
+    const inputType = 'water';
+
+    const nameRadio = screen.getByTestId(nameRadioId);
+
+    const searchButton = screen.getByRole('button', { name: /search/i });
+
+    // verifica se a rota mudou para a rota de detalhes da comida
+
+    await act(async () => {
+      await user.type(searchInput, inputType);
+      await user.click(nameRadio);
+      await user.click(searchButton);
+    });
+
+    // espera mudar de rota.
+    await waitFor(() => expect(window.location.pathname).toBe('/drinks/178332'));
+
+    expect(await screen.findByRole('heading', { name: /smashed watermelon margarita/i })).toBeInTheDocument();
+  });
+
+  it('Verifica se ao passar um ingrediente que não existe no SearchBar, é exibido um alerta, na pagina Drinks', async () => {
+    const alertMock = vi.spyOn(window, 'alert').mockImplementation(() => {});
+    const { user } = renderWithRouter(
+      <SearchBarProvider>
+        <App />
+      </SearchBarProvider>,
+      { route: '/drinks' },
+    );
+
+    const searchBtn = screen.getByRole('img', { name: /pesquisar/i });
+
+    await user.click(searchBtn);
+
+    const searchInput = screen.getByTestId(search);
+
+    const inputInvalid = 'xablau';
+
+    const nameRadio = screen.getByTestId(nameRadioId);
+
+    const searchButton = screen.getByRole('button', { name: /search/i });
+
+    await act(async () => {
+      await user.type(searchInput, inputInvalid);
+      await user.click(nameRadio);
+      await user.click(searchButton);
+    });
+
+    await waitFor(() => expect(alertMock).toHaveBeenCalledWith(
+      "Sorry, we haven't found any recipes for these filters",
+    )); // verifica se apareceu o alerta
+  });
+
+  it('Testa se o SearchBar na pagina "drinks" quando passado mais de uma letra retorna um alert', async () => {
+    const alertMock = vi.spyOn(window, 'alert').mockImplementation(() => {});
+    const { user } = renderWithRouter(
+      <SearchBarProvider>
+        <App />
+      </SearchBarProvider>,
+      { route: '/drinks' },
+    );
+    const searchBtn = screen.getByRole('img', { name: /pesquisar/i });
+
+    await user.click(searchBtn);
+
+    const searchInput = screen.getByTestId(search);
+
+    const inputInvalid = 'xablau';
+
+    const firstLetterRadio = screen.getByTestId(firstLetterRadioId);
+
+    const searchButton = screen.getByRole('button', { name: /search/i });
+
+    await act(async () => {
+      await user.type(searchInput, inputInvalid);
+      await user.click(firstLetterRadio);
+      await user.click(searchButton);
+    });
+
+    await waitFor(() => expect(alertMock).toHaveBeenCalledWith(
+      'Your search must have only 1 (one) character',
+    )); // verifica se apareceu o alerta
+  });
+
+  it('Testa se o SearchBar na pagina "meals" quando passado mais de uma letra retorna um alert', async () => {
+    const alertMock = vi.spyOn(window, 'alert').mockImplementation(() => {});
+    const { user } = renderWithRouter(
+      <SearchBarProvider>
+        <App />
+      </SearchBarProvider>,
+      { route: '/meals' },
+    );
+    const searchBtn = screen.getByRole('img', { name: /pesquisar/i });
+
+    await user.click(searchBtn);
+
+    const searchInput = screen.getByTestId(search);
+
+    const inputInvalid = 'xablau';
+
+    const firstLetterRadio = screen.getByTestId(firstLetterRadioId);
+
+    const searchButton = screen.getByRole('button', { name: /search/i });
+
+    await act(async () => {
+      await user.type(searchInput, inputInvalid);
+      await user.click(firstLetterRadio);
+      await user.click(searchButton);
+    });
+
+    await waitFor(() => expect(alertMock).toHaveBeenCalledWith(
+      'Your search must have only 1 (one) character',
+    )); // verifica se apareceu o alerta
   });
 });
